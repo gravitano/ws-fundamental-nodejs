a5f86167ddd005a9e328da965a0ba838aa29b4e6
Warsono <warsono16694@gmail.com>
2019-10-28 01:53:32 +0700

    added variable example

---
+++ 01-nodejs-javascript/variable.js
@@ -0,0 +1,24 @@
+// string
+var hello = "Hello";
+let world = "World";
+// Concatenation
+const helloWorld = "Hello" + " " + world;
+const myString = new String("This is a string");
+// number
+const age = 20;
+const yourAge = new Number(25);
+// boolean
+const isLoggedIn = true;
+const hasProfile = null;
+// object
+const myObject = {
+  hello: "World"
+};
+// array
+const myArray = [1, 2, 3];
+// output
+console.log(helloWorld, myString);
+console.log(age, yourAge);
+console.log(isLoggedIn, hasProfile);
+console.log(myObject);
+console.log(myArray);
