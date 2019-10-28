44267fb0e100492b135b13c3fb2e7995bf417b04
Warsono <warsono16694@gmail.com>
2019-10-28 23:38:09 +0700

    added express middleware example

---
+++ 05-express-middleware/app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const myLogger = function(req, res, next) {
+  console.log("LOGGED");
+  next();
+};
+
+app.use(myLogger);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/", (req, res) => {
+  res.send("Got a POST request");
+});
+
+app.put("/user", (req, res) => {
+  res.send("Got a PUT request at /user");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Got a DELETE request at /user");
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
