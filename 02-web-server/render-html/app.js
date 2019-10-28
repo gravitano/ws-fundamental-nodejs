d9baf5e073f7eb3c4c901e9049ba2f878eaa0edf
Warsono <warsono16694@gmail.com>
2019-10-28 22:22:56 +0700

    remove unsed fun

--- 02-web-server/render-html/app.js
+++ 02-web-server/render-html/app.js
@@ -4,8 +4,6 @@ const fs = require("fs");
 const hostname = "127.0.0.1";
 const port = 3000;
 
-const getView = () => fs.readFileSync(__dirname + "/index.html");
-
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/html");
