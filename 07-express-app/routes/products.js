84ec19484050df500f8e12d5d4fc7ab505d8b09f
Warsono <warsono16694@gmail.com>
2019-10-29 01:45:24 +0700

    implement edit product

--- 07-express-app/routes/products.js
+++ 07-express-app/routes/products.js
@@ -23,8 +23,16 @@ router.get("/:id", async function(req, res, next) {
     data: product
   });
 });
-router.put("/:id", function(req, res, next) {
-  res.send("Update product");
+router.put("/:id", async function(req, res, next) {
+  const id = req.params.id;
+  const data = req.body;
+  console.log(id);
+  await model.Product.update(data, {
+    where: { id }
+  });
+  res.json({
+    data
+  });
 });
 router.delete("/:id", function(req, res, next) {
   res.send("Delete product");
