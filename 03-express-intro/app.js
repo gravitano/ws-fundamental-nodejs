42701711ef2071dd58d9ffc25f33ad49771b1eea
Warsono <warsono16694@gmail.com>
2019-10-28 23:33:56 +0700

    added routing example

--- 03-express-intro/app.js
+++ 03-express-intro/app.js
@@ -2,6 +2,20 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/", (req, res) => {
+  res.send("Got a POST request");
+});
+
+app.put("/user", (req, res) => {
+  res.send("Got a PUT request at /user");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Got a DELETE request at /user");
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
