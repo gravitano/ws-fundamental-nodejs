bbd8e000f0b764566a33277b8f00c242a7d274b1
Warsono <warsono16694@gmail.com>
2019-10-28 22:36:30 +0700

    refactor switch into getRoute function

--- 02-web-server/simple-routing/app.js
+++ 02-web-server/simple-routing/app.js
@@ -4,32 +4,34 @@ const fs = require("fs");
 const hostname = "127.0.0.1";
 const port = 3000;
 
+const getRoute = (req, res) => {
+  let route = "";
+  switch (req.url) {
+    case "/":
+      route = "index";
+      break;
+    case "/contact":
+      route = "contact";
+      break;
+    case "/about":
+      route = "about";
+      break;
+    default:
+      res.statusCode = 404;
+      route = "404";
+      break;
+  }
+  return route;
+};
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/html");
 
-  let content = "";
-  let file = "";
-
   console.log("URL", req.url);
 
-  switch (req.url) {
-    case "/":
-      file = "index";
-      break;
-    case "/contact":
-      file = "contact";
-      break;
-    case "/about":
-      file = "about";
-      break;
-    default:
-      res.statusCode = 404;
-      file = "404";
-      break;
-  }
-
-  content = fs.readFileSync(`${__dirname}/${file}.html`);
+  const route = getRoute(req, res);
+  const content = fs.readFileSync(`${__dirname}/${route}.html`);
 
   res.end(content);
 });
