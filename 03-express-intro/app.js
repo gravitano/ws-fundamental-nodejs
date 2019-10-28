04bdb40ccf5922bee32edc833e2958d3ed560df4
Warsono <warsono16694@gmail.com>
2019-10-28 23:36:53 +0700

    revert intro

--- 03-express-intro/app.js
+++ 03-express-intro/app.js
@@ -6,16 +6,4 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.post("/", (req, res) => {
-  res.send("Got a POST request");
-});
-
-app.put("/user", (req, res) => {
-  res.send("Got a PUT request at /user");
-});
-
-app.delete("/user", (req, res) => {
-  res.send("Got a DELETE request at /user");
-});
-
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
